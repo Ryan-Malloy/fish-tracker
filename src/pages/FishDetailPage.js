36e3a31e9b424b0ca9267f685d834953799451ea
RyanMalloy <129435196+RyanMalloy@users.noreply.github.com>
2023-11-06 13:53:29 -0500

    table added to fish detail page

--- src/pages/FishDetailPage.js
+++ src/pages/FishDetailPage.js
@@ -19,123 +19,56 @@ function FishDetailPage({ fishes }) {
 		fetchFish();
 	}, [id]);
 
-	const getDelta = (property) => {
-		if (!fish.caught || fish.caught.length < 2) return null;
-		const mostRecent = fish.caught[fish.caught.length - 1][property];
-		const previous = fish.caught[fish.caught.length - 2][property];
-		const delta = mostRecent - previous;
-
-		return delta !== 0 ? delta : null;
-	};
-
 	return (
-		<>
-			<section>
-				<div className="container mx-auto">
-					<div>
-						{fish ? (
-							<div className="card mb-3 mx-auto">
-								<div className="card-header">
-									<div className="row">
-										<div className="col">
-											<h3 className="card-title">{fish.name}</h3>
-											<p className="text-secondary">Type: {fish.type}</p>
-										</div>
-										<div className="col-auto">
-											<small className="text-secondary">{fish.key}</small>
-										</div>
-									</div>
-								</div>
-								<div className="card-body">
-									<div className="row">
-										<div className="col">
-											<p>
-												<b>Weight:</b>{" "}
-												{fish.caught && fish.caught.length > 0
-													? fish.caught[fish.caught.length - 1].weight + " oz"
-													: "N/A"}
-												{getDelta("weight") && (
-													<span
-														className={
-															getDelta("weight") > 0
-																? "text-success"
-																: "text-danger"
-														}
-													>
-														({getDelta("weight") > 0 ? "+" : ""}
-														{getDelta("weight")} oz)
-													</span>
-												)}
-											</p>
-										</div>
-										<div className="col-auto">
-											<p>
-												<b>Length:</b>{" "}
-												{fish.caught && fish.caught.length > 0
-													? fish.caught[fish.caught.length - 1].length + " cm"
-													: "N/A"}
-												{getDelta("length") && (
-													<span
-														className={
-															getDelta("length") > 0
-																? "text-success"
-																: "text-danger"
-														}
-													>
-														({getDelta("length") > 0 ? "+" : ""}
-														{getDelta("length")} cm)
-													</span>
-												)}
-											</p>
-										</div>
-									</div>
-									<hr></hr>
-									<h5>Catches</h5>
-									{fish.caught && (
-										<div>
-											{[...fish.caught].reverse().map((catchDetails, index) => (
-												<div key={index}>
-													<a
-														data-bs-toggle="collapse"
-														href={`#collapseFish${fish.key}Catch${index}`}
-														aria-expanded="true"
-														aria-controls={`collapseFish${fish.key}Catch${index}`}
-													>
-														Date: {catchDetails.date}
-													</a>
-													<ul
-														className="collapse show"
-														id={`collapseFish${fish.key}Catch${index}`}
-													>
-														<li>
-															<b>Weight:</b> {catchDetails.weight}
-														</li>
-														<li>
-															<b>Length:</b> {catchDetails.length}
-														</li>
-														<li>
-															<b>Location:</b> {catchDetails.location}
-														</li>
-														<li>
-															<b>Lure:</b> {catchDetails.lure}
-														</li>
-													</ul>
-												</div>
-											))}
-										</div>
-									)}
-								</div>
+		<section>
+			<div className="mx-auto p-1">
+				{fish ? (
+					<>
+						<div className="row">
+							<div className="col">
+								<h3>{fish.name}</h3>
+								<p className="text-secondary">{fish.type}</p>
 							</div>
-						) : (
-							<p className="text-center mt-5">Loading fish details...</p>
-						)}
-					</div>
-					<p>
-						<a href="/">Back to Home</a>
-					</p>
-				</div>
-			</section>
-		</>
+							<div className="col-auto">
+							<small className="text-secondary">ID: {fish.key}</small>
+							</div>
+						</div>
+
+						
+						<table className="table table-striped">
+							<thead>
+								<tr>
+									<th scope="col">Catch</th>
+									<th scope="col">Date</th>
+									<th scope="col">Weight</th>
+									<th scope="col">Length</th>
+									<th scope="col">Location</th>
+									<th scope="col">Lure</th>
+								</tr>
+							</thead>
+							<tbody>
+								{fish.caught &&
+									[...fish.caught].map((catchDetails, index) => (
+										<tr key={index}>
+											<th scope="row">{index+1}</th>
+											<td>{catchDetails.date}</td>
+											<td>{catchDetails.weight} oz</td>
+											<td>{catchDetails.length} cm</td>
+											<td>{catchDetails.location}</td>
+											<td>{catchDetails.lure}</td>
+										</tr>
+									))}
+							</tbody>
+						</table>
+					</>
+				) : (
+					<p className="text-center mt-5">Loading fish details...</p>
+				)}
+				<p>
+					<a href="/">Back to Home</a>
+				</p>
+			</div>
+		</section>
 	);
 }
 
